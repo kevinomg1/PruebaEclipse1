c46ac734eb7d8920303bd65a0fedc102446c4773
kevinmerino <kevin.merino.ga@gmail.com>
2022-07-27 11:12:40 -0500

    Agregadas variables

---
+++ src/EjemploGit.java
@@ -0,0 +1,13 @@
+
+public class EjemploGit {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hola alumno de Git");	
+		
+		String nombre = "Juan Diaz";
+		
+		}
+
+}
