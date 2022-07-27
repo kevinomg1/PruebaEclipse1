1b0c2fe3cd1ef14b8e73c3a0b61182f96396ce8f
kevinmerino <kevin.merino.ga@gmail.com>
2022-07-27 14:28:26 -0500

    Agregando variables numericas

--- src/EjemploGit.java
+++ src/EjemploGit.java
@@ -7,8 +7,12 @@ public class EjemploGit {
 		System.out.println("Hola alumno de Git");	
 		
 		String nombre = "Juan Diaz";
+		int x=5;
 		
+		x=15*20;
 		System.out.println(nombre);
+		System.out.println(x);
+		
 		}
 
 }
